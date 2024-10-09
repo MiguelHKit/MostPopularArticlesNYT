24b3c5624ada2f3038beb9ab90ee1ca34261a728
Mike <emikelogor7@icloud.com>
2024-10-09 12:36:06 -0600

    Initial Commit

---
+++ PruebaTecnicaRaven/PruebaTecnicaRavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaTecnicaRavenApp.swift
+//  PruebaTecnicaRaven
+//
+//  Created by Miguel T on 09/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PruebaTecnicaRavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
